9395e1322afae111c273ac1aec5de2e9bcf9f802
nazmulhasan <nazmul@apurbatech.com>
2015-04-27 15:19:11 +0600

    Contact Us form is completed

--- contact_us_post.php
+++ contact_us_post.php
@@ -2,21 +2,60 @@
 
 
 
-    $fname  = $_REQUEST['fname'];
-
-    $lname  = $_REQUEST['lname'];
-
-    //$name  = $_REQUEST['name'];
-
+    $fname  = $_REQUEST['first_name'];
+    $lname  = $_REQUEST['last_name'];
     $email = $_REQUEST['email'];
-
+    
+    $country = $_REQUEST['country'];
+    $country_code = $_REQUEST['country_code'];
+    $phone = $_REQUEST['phone'];
+    $website = $_REQUEST['website'];
+    
+    $company_name = $_REQUEST['company_name'];
+    $company_type = "";
+    if(isset($_REQUEST['company_type']))
+    {
+        $company_type_id = $_REQUEST['company_type'][0];
+        $company_type = $company_type_id == 0 ? "Termination Provider":$company_type;
+        $company_type = $company_type_id == 1 ? "Wholesaler / Reseller":$company_type;
+        $company_type = $company_type_id == 2 ? "Retailer":$company_type;
+        $company_type = $company_type_id == 3 ? "Other":$company_type;
+    }    
+    $daily_revenue = "";
+    if(isset($_REQUEST['daily_revenue']))
+    {
+        $daily_revenue_id = $_REQUEST['daily_revenue'][0];
+        $daily_revenue = $daily_revenue_id == 0 ? "Under $1,000":$daily_revenue;
+        $daily_revenue = $daily_revenue_id == 1 ? "$1,000 - $10,000":$daily_revenue;
+        $daily_revenue = $daily_revenue_id == 2 ? "$10,000 - $50,000":$daily_revenue;
+        $daily_revenue = $daily_revenue_id == 3 ? "$50,000 +":$daily_revenue;
+    }
+    
+    $im_type = "";
+    $im_selection_id = $_REQUEST['im_selection'];
+    $im_type = $im_selection_id == 0 ? "AIM":$im_type;
+    $im_type = $im_selection_id == 1 ? "Gtalk":$im_type;
+    $im_type = $im_selection_id == 2 ? "MSN":$im_type;
+    $im_type = $im_selection_id == 3 ? "Skype":$im_type;
+    $im_type = $im_selection_id == 4 ? "Yahoo":$im_type;
+    $im = $_REQUEST['im'];    
     $desc  = $_REQUEST['message'];
-
-
 
     $emailSubject = 'Fonebox Website - Mail';
 
-    $emailMessage = $desc;
+    $emailMessage = "";
+    $emailMessage = $emailMessage."Country:".$country."<br/>";
+    $emailMessage = $emailMessage."Phone:".$country_code.$phone."<br/>";
+    $emailMessage = $emailMessage."Website:".$website."<br/>";
+    $emailMessage = $emailMessage."Company Name:".$company_name."<br/>";
+    $emailMessage = $emailMessage."Company Type:".$company_type."<br/>";
+    $emailMessage = $emailMessage."Daily Revenue:".$daily_revenue."<br/>";
+    $emailMessage = $emailMessage."IM:".$im_type.":".$im."<br/>";
+    
+    $emailMessage = $emailMessage.$desc;
+    
+    print_r($emailMessage);
+    return;
 
     $emailTo = 'ask@fonebox.com.sg';
 
